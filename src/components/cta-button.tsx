d36d181ae3382924ce06dcddd94c08c42931a185
afrocan1 <114925535+afrocan1@users.noreply.github.com>
2025-11-10 16:36:54 +0100

    Fix button link and correct spelling error

--- src/components/cta-button.tsx
+++ src/components/cta-button.tsx
@@ -9,10 +9,10 @@ export function CTAButton() {
   return (
     <div className="button-wrapper">
       <button
-        onClick={() => router.push('https://contate.me/bewave')}
+        onClick={() => router.push('')}
         className="w-[calc(100%-2px)] h-[calc(100%-2px)] absolute z-30 top-[1px] left-[1px] text-sm bg-black flex items-center justify-center gap-5 rounded-3xl focus:outline-none"
       >
-        Distributw
+        Distribute
         <FiArrowRight size={18} />
       </button>
     </div>
